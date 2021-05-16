82728dc7be81c4f66f80fbaf30d289549dd8dd45
Thoo <40025225+thoo0224@users.noreply.github.com>
2021-05-16 21:45:28 +0200

    o

--- tests/Fortnite.Net.Test/Program.cs
+++ tests/Fortnite.Net.Test/Program.cs
@@ -2,11 +2,13 @@
 
 namespace Fortnite.Net.Test
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
     }
 }
